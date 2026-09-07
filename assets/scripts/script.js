9904774180c6fe42e1b54ac35298ccbd7f46b2b6
igarin14pm <145533797+igarin14pm@users.noreply.github.com>
2026-09-07 17:13:12 +0900

    "何か" を追加

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -76,3 +76,21 @@ const rohdomopoApp = new RohdomopoApp(
 window.addEventListener('DOMContentLoaded', () => {
   rohdomopoApp.initialize();
 });
+
+// これは何だ？？？
+function doSomething(str) {
+  const bi = atob(str);
+  const by = Uint8Array.from(bi, (m) => m.codePointAt(0));
+  return new TextDecoder().decode(by);
+}
+console.info(
+  doSomething('JWPwn5GB77iPIPCfkYHvuI8='),
+  doSomething('Zm9udC1zaXplOiA2NHB4')
+);
+console.info(
+  doSomething('JWMi44Kz44Oz44K944O844Or44KS6KaX44GP44Go44GN44CB44Kz44Oz44K944O844Or44KC44G+44Gf44GT44Gh44KJ44KS6KaX44GE44Gm44GE44KL44Gu44Gg44CCIg=='),
+  doSomething('Zm9udC1zaXplOiAxNnB4OyBmb250LXN0eWxlOiBpdGFsaWM7IGZvbnQtd2VpZ2h0OiA2MDA7')
+);
+console.info(
+  doSomething('8J+klPCfkq0g44KP44GW44KP44GW44Kz44Oz44K944O844Or44KS6ZaL44GP44Gq44KT44Gm44CB44KC44GX44GL44GX44Gm44CB44K944O844K544Kz44O844OJ44Gr6IiI5ZGz44GM44GK44GC44KK44Gn44GZ44Gt77yf77yf44GT44Gh44KJ44GL44KJ44Gp44GG44Ge77yB77yBDQrwn5GH8J+Pu/CfkYfwn4+78J+Rh/Cfj7sNCmh0dHBzOi8vZ2l0aHViLmNvbS9pZ2FyaW4xNHBtL3JvaGRvbW9wbw==')
+);
